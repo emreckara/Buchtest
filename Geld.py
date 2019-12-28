3e08cf92e442e58a5fa32c1ecb9d7ea0a6f71937
emreckara <44549623+emreckara@users.noreply.github.com>
2019-12-28 14:28:35 +0100

    added turkish lira

--- Geld.py
+++ Geld.py
@@ -3,6 +3,7 @@ class Geld(object):
                  'GBP':1.39480,
                  'EUR':1.0,
                  'JPY':0.007168
+                 'TL': 0.15  
                  }
     def __init__(self, waehrung, betrag):
         self.waehrung = waehrung
